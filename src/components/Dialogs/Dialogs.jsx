bba79218b6c46841bf898901dd93e475f964fc95
Kiterina <webdevvva@gmail.com>
2024-08-21 19:03:58 +0300

    done lesson 20

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,7 @@ import css from './Dialogs.module.css'
 const Dialogs = (props) => {
     return (
         <div className={css.dialogs_wrapper}>
-        dialogsg
+        dialogs
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import css from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = (props) => {
     return(
         <nav className={css.nav}>
-            <div className={`${css.item} ${css.active}`}>
-                <a href='/profile'>Profile</a>
+            <div className={`${css.item}`}>
+                <NavLink to='/profile' className={({isActive}) => isActive ? `${css.active}` : ''}>Profile</NavLink>
             </div>
             <div className={css.item}>
-                <a href='/dialogs'>Messages</a>
+                <NavLink to='/dialogs' className={({isActive}) => isActive ? `${css.active}` : ''}>Messages</NavLink>
             </div>
             <div className={css.item}>
-                <a href='/news'>News</a>
+                <NavLink to='/news' className={({isActive}) => isActive ? `${css.active}` : ''}>News</NavLink>
             </div>
             <div className={css.item}>
-                <a href='/music'>Music</a>
+                <NavLink to='/music' className={({isActive}) => isActive ? `${css.active}` : ''}>Music</NavLink>
             </div>
         </nav>
     )
